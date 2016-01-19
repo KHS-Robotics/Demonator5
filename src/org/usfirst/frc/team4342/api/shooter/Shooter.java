1ff36c448993f9c664b078e5a423c7b69c32ff0e
Payton <pytondouglas00@aol.com>
2016-01-19 17:47:31 -0500

    add shooter skeleton

---
+++ src/org/usfirst/frc/team4342/api/shooter/Shooter.java
@@ -0,0 +1,25 @@
+ package org.usfirst.frc.team4342.api.shooter;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class Shooter 
+	{
+
+	public Shooter(Joystick j, CANTalon a, CANTalon b)
+	{
+		
+	}
+	
+	public void accumulate() 
+	{
+	
+		
+	}
+	
+	public void shoot() 
+	{
+		 
+		
+	}
+}
